805954e3332d04c7f0db3c5b6857d134f3cb4fd8
DatTVu <dtvutrong1993@gmail.com>
2022-08-22 03:00:11 +0700

    exercise 1.7

--- part1/src/App.js
+++ part1/src/App.js
@@ -17,28 +17,45 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [all, setAll] = useState(0)
+  const [percentage, setPercentage] = useState(0)
+  const [average, setAverage] = useState(0)
+
   const title = "Give feedback"
   const goodButtonTitle = "good"
   const badButtonTitle = "bad"
   const neutralButtonTitle = "neutral"
   const statisticsHeader = "Statistics"
+
   const handleGoodState = () => {
     const newGood = good + 1
     const newAll = all + 1
+    const newPercentage = newGood * 100 / newAll 
+    const newAverage = (newGood - bad) / newAll
     setAll(newAll)
     setGood(newGood)
+    setPercentage(newPercentage)
+    setAverage(newAverage)
   }
+
   const handleBadState = () => {
     const newBad = bad + 1
     const newAll = all + 1
+    const newPercentage = good * 100 / newAll
+    const newAverage = (good - newBad) / newAll
     setAll(newAll)
     setBad(newBad)
+    setPercentage(newPercentage)
+    setAverage(newAverage)
   }
   const handleNeutralState = () => {
     const newNeutral = neutral + 1
     const newAll = all + 1
+    const newPercentage = good * 100/ newAll
+    const newAverage = (good - bad) / newAll
     setAll(newAll)
     setNeutral(newNeutral)
+    setPercentage(newPercentage)
+    setAverage(newAverage)
   }
   return (
     <div>
@@ -51,6 +68,8 @@ const App = () => {
       <Display text="Bad" state={bad}/>
       <Display text="Neutral" state={neutral}/>
       <Display text="All" state={all}/>
+      <Display text="Average" state={average}/>
+      <Display text="Percentage: " state={percentage}/>
     </div>
   )
   
